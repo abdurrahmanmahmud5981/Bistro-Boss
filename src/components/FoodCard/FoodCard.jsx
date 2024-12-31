848deccd2a4d70a0d1a7225c05b118dc2d8dcd46
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-12-31 21:21:22 +0600

    update FoodCard styling and button design; modify useMenu hook to fetch data from local server

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -12,11 +12,11 @@ const FoodCard = ({item}) => {
           />
         </figure>
         <p className="bg-slate-900 absolute right-2 top-2 p-2 rounded-md text-white">${price}</p>
-        <div className="card-body">
+        <div className="card-body flex flex-col items-center">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Add to Cart</button>
+            <button className="btn btn-outline border-0 border-orange-400 bg-slate-100 border-b-4">Add to Cart</button>
           </div>
         </div>
       </div>
